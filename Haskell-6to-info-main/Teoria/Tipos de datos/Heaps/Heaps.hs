316595c97e4ed9042a42aff93ca35e2da66a4a24
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-05-14 21:47:36 -0300

    Add files via upload

---
+++ Haskell-6to-info-main/Teoria/Tipos de datos/Heaps/Heaps.hs
@@ -0,0 +1,69 @@
+--
+-- Heaps comunes
+--
+-- Arboles que permiten acceso eficiente al minimo elemento
+
+-- Invariantes:
+-- Todo nodo es menor a todos los valores de sus hijos 
+-- Por lo anterior, el minimo siempre estará en la raiz
+
+-- Operaciones tipicas:
+-- insert :: Ord a => a -> Heap a -> Heap a
+-- findMin :: Ord a => Heap a -> a
+-- deleteMin :: Ord a => Heap a -> Heap a
+
+
+
+--
+-- Leftist Heaps
+--
+
+--                       1
+--           7                       3
+--   10              9
+--               12      11
+-- Heaps con una invariante mas:
+-- º El rango de cualquier hijo izquierdo es mayor o igual que el de su hermano de la derecha.
+-- Para explicar esto:
+-- Rango es la longitud de la espina derecha de cada nodo.
+-- La espina derecha es la ruta más corta a una hoja. 
+-- Como consecuencia los elementos de la espina derecha están ordenados
+--
+-- Tiene todas las operaciones de Heaps y una adicional: 
+-- merge :: Ord a => Heap a -> Heap a -> Heap a (la union entre dos heaps)
+
+type Rank = Int
+data Heap a = E | N Rank a (Heap a) (Heap a)
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge h1 E = h1
+merge E h2 = h2
+merge h1@(N _ x a1 b1) h2@(N _ y a2 b2) = -- el @ sirve para decir sinonimos, es decir, h1 = N _ x a1 b1
+                            if x <= y then makeH x a1 (merge b1 h2)
+                            else makeH y a2 (merge h1 b2) -- se pone al menor 
+-- Decidido quien es la nueva ra´ız, debemos determinar los dos sub-´arboles
+-- que la acompa˜nen:
+-- Las espinas derechas (b) se mezclan (merge) para seguir ordenadas y
+-- preservar la invariante leftist. Para preservarla definimos makeH.
+
+rank :: Heap a -> Rank -- devuelve el rango de un heap
+rank E = 0
+rank (N r _ _ _ ) = r
+
+-- Crea un heap
+makeH x a b = if rank a >= rank b   then N (rank b + 1) x a b
+                                    else N (rank a + 1) x b a
+
+-- Tanto rank como makeH no son recursivas.
+-- Como la espina derecha es logaritmica, merge es logaritmica.
+
+-- Si se define bien el merge, el resto de operaciones es muy simple.
+
+insert:: Ord a => a -> Heap a -> Heap a 
+insert x h = merge (N 1 x E E) h
+
+findMin :: Ord a => Heap a -> a -- super simple, el minimo siempre estara en la raiz.
+findMin (N _ x a b) = x 
+
+deleteMin :: Ord a => Heap a -> Heap a -- borras la raiz y mergeas.
+deleteMin (N _ x a b) = merge a b 
---
+++ Haskell-6to-info-main/Teoria/Tipos de datos/RedBlackTree/RedBlackTree.hs
@@ -0,0 +1,56 @@
+data Color = R | B deriving Show -- Sirve para que se pueda mostrar el deriving show
+data RBT a = E | T Color (RBT a) a (RBT a) deriving Show
+-- E significa Empty, T significa tree
+colcha = T B ( T R ( T B E 1 E) 2 ( T B E 3 E) ) 5 ( T R ( T B E 6 E ) 7 ( T B E 8 E ))
+-- Ejemplo de     
+--       5
+--   2       7
+-- 1   3   6   8
+
+-- Invariantes: 
+-- 1) Todos los nodos rojos tienen un padre negro. (Invariante Local)
+-- 2) Todos los caminos de la raız a una hoja tienen el mismo numero de
+-- nodos negros (altura negra). (Invariante Global) => balanceo
+
+-- Esto significa que la altura siempre esta en el orden del log n 
+-- (siendo n la cantidad de elementos + 1)
+
+-- La siguiente funcion te muestra si algo es miembro
+memberRBT :: Ord a => a -> RBT a -> Bool -- Ord fuerza que todos los valores se puedan comparar
+memberRBT a E = False
+memberRBT a (T _ l b r)     | a == b = True
+                            | a < b = memberRBT a l
+                            | a > b = memberRBT a r
+
+-- La siguiente funcion inserta algo
+insertRTB :: Ord a => a -> RBT a -> RBT a
+insertRTB x t = makeBlack (ins x t)
+    where   ins x E                   = T R E x E
+            ins x (T c l y r)   | x < y     = balanceRTB c (ins x l) y r
+                                | x > y     = balanceRTB c l y (ins x r)
+                                | otherwise = T c l y r
+
+
+-- Para mantener la (altura negra) el nuevo nodo se inserta rojo. (Global)
+-- Pero esto puede romper la Propiedad 1,para solucionar esto vamos a rebalancear. (Local)
+-- El rebalanceo puede dejarnos una raiz roja, asi que la coloreamos de negro.
+
+-- Luego de insertar el nuevo nodo rojo hay a lo sumo una ´unica violaci´on de
+-- la propiedad 1, que ocurre cuando el padre es rojo.
+-- Esta violacion, puede aparecer en cuatro configuraciones:
+-- z x y
+
+-- Esta es la funcion que transforma un nodo de color x a un nodo de color B(lack)
+makeBlack E = E
+makeBlack (T _ l x r) = T B l x r
+
+-- Esta es la funcion del balanceo
+balanceRTB :: Color -> RBT a -> a -> RBT a -> RBT a
+balanceRTB B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
+balanceRTB B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
+balanceRTB B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+balanceRTB B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
+balanceRTB c l a r = T c l a r
+
+-- Es una implementacion no recursiva.
+-- El costo de insert es logaritmico.
