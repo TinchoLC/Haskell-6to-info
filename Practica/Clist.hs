36e672993dd01a961e08965d14ee2e0a2b26352a
Giuliana Grieco <104744753+ggiuliana@users.noreply.github.com>
2023-05-20 17:05:33 -0300

    funciones de acceso
    las hicimos en clase 👍

--- Practica/Clist.hs
+++ Practica/Clist.hs
@@ -1,1 +1,14 @@
 data Clist a = EmptyCL | CUnit a | Consnoc a (Clist a) a
+
+--funciones de acceso:
+headCL (CUnit x) = x
+headCL (Consnoc x xs y) = x
+
+tailCL (CUnit x) = EmptyCL
+tailCL (Consnoc x xs y) = Consnoc (headCL xs) (tailCL xs) y
+
+isEmptyCL EmptyCL = True
+isEmptyCL _ = False
+
+isCUnit (CUnit _) = True
+isCUnit _ = False
