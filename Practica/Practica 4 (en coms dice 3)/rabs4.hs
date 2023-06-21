cb8aa484134b5b1ce23e53f6be47b7d6c109ae66
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-06-21 14:05:18 -0300

    Update rabs4.hs

--- Practica/Practica 4 (en coms dice 3)/rabs4.hs
+++ Practica/Practica 4 (en coms dice 3)/rabs4.hs
@@ -1,1 +1,7 @@
-si
+Practica 3 (segun titulo en comunidades)
+
+-- 3) ¿Cuales de los siguientes enunciados son verdaderos? Probar las respuestas.
+-- 1. n^2 ∈ O(n^3)
+-- 2. n^2 ∈ Ω(n^3)
+-- 3. 2^n ∈ Θ(2^(n+1))
+-- 4. n! ∈ Θ((n + 1)!)
